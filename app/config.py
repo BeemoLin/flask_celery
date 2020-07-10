d619e7baf8cbbe49666d40679f7baea8732da508
BeemoLin <BeemoLin@users.noreply.github.com>
2020-07-10 10:59:23 +0800

    Update config.py

--- app/config.py
+++ app/config.py
@@ -2,7 +2,21 @@ import os
 
 
 class Config:
-    pass
+
+import os
+
+
+class Config:
+    # celery config
+
+    CELERY_CONFIG = {
+        'timezone': 'Asia/Taipei'
+        'result_accept_content': ['json']
+        'task_serializer': 'json'
+        'result_serializer': 'json'
+        'result_backend': "redis://{}:6379".format(os.environ.get("REDIS_HOST", ''))
+        'broker_url': "redis://{}:6379".format(os.environ.get("REDIS_HOST", ''))
+    }
 
 
 class DevelopmentConfig(Config):
